1d6f1dcee8d275b07b30c9ebf796301298ed45fe
Eleazar Federio <eleazarfederio@rocketmail.com>
2018-05-13 00:54:45 +0800

    Learning ruby on rails from basic day 2, need more hours to learn, time management goal

--- theassociation/app/controllers/players_controller.rb
+++ theassociation/app/controllers/players_controller.rb
@@ -8,16 +8,28 @@ class PlayersController < ApplicationController
 
     end
 
+    def show
+        @players = Player.find(params[:id])
+    end
+
     def create
-        player = Player.new
-        player.first_name = params[:player][:first_name]
-        player.last_name = params[:player][:last_name]
+
+        # player = Player.new
+        # player.first_name = params[:player][:first_name]
+        # player.last_name = params[:player][:last_name]
+        
+        player = Player.new(player_params)
+
         if player.save 
             redirect_to "/players"
         else
             flash[:errors] = player.errors.full_messages
             redirect_to "/players/new"
         end
+    end
+
+    private def player_params
+        params.require(:player).permit(:first_name, :last_name)
     end
 
 end
